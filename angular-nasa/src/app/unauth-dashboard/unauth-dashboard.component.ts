29d68121f47903be5961d99be7f4abda46412a77
Anthony Fekete <afekete3@uwo.ca>
2017-12-04 03:17:53 +0000

    final update

--- angular-nasa/src/app/unauth-dashboard/unauth-dashboard.component.ts
+++ angular-nasa/src/app/unauth-dashboard/unauth-dashboard.component.ts
@@ -42,7 +42,7 @@ export class UnauthDashboardComponent implements OnInit {
     var len = arr.length;
      for (var i = len-1; i>=0; i--){
        for(var j = 1; j<=i; j++){
-         if(arr[j-1]['rank']<arr[j]['rank']){
+         if(arr[j-1]['rank'].length<arr[j]['rank'].length){
              var temp = arr[j-1];
              arr[j-1] = arr[j];
              arr[j] = temp;
